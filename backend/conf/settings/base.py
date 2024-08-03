12cb59fb8180876f290ebe0b844d2b15167d53a1
jslee <dlwhdtjd098@gmail.com>
2024-08-03 17:00:33 +0900

    modify(conf): database 설정 추가

--- backend/conf/settings/base.py
+++ backend/conf/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,8 +83,12 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # 작업 주의사항 : sqlite가 아닌 MySQL 설정으로 변경해 주세요
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME", "contactbook"),
+        "USER": os.environ.get("DB_USER", "mysql"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "mysql"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "3306"),
     }
 }
 
