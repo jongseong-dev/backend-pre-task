35f05eb54f9ed2323be3054b2237e04fe78e4437
jslee <dlwhdtjd098@gmail.com>
2024-08-05 17:27:59 +0900

    refactor: 공통되는 serializer를 수정함

--- backend/apps/contactbook/serializers.py
+++ backend/apps/contactbook/serializers.py
@@ -1,32 +1,67 @@
 from django.db import transaction
-from phonenumber_field.serializerfields import (
-    PhoneNumberField,
-)
 from rest_framework import serializers
 
 from apps.contactbook.models import ContactBook, ContactLabel
 from apps.label.models import Label
-from apps.label.serializers import LabelListSerializer
+from apps.label.serializers import LabelSerializer
 
 
 # TODO: 1. Serializer 정리
 # TODO: 2. 중복되는 로직을 service layer로 이동시켜야 한다.
-class ContactBookListSerializer(serializers.ModelSerializer):
-    profile_image_url = serializers.URLField(
-        required=False, help_text="프로필 이미지 URL"
+class ContactLabelSerializer(serializers.Serializer):
+    """
+    연락처에서 필요한 라벨에 대한 정보를 담고있는 Serializer
+    1. write_only: label_ids - 라벨 ID를 입력으로 받는다.
+    2. read_only: labels - 라벨 정보를 중첩된 구문으로 보여준다.
+    """
+
+    labels = serializers.SerializerMethodField(help_text="라벨 목록")
+    label_ids = serializers.ListField(
+        write_only=True,
+        required=False,
+        child=serializers.IntegerField(),
+        help_text="라벨 ID",
     )
+
+    def get_labels(self, obj) -> list[dict]:
+        label_ids = obj.labeled_contact.values_list("label_id", flat=True)
+        queryset = Label.objects.filter(id__in=label_ids)
+        return LabelSerializer(queryset, many=True).data
+
+    class Meta:
+        fields = ["labels", "label_ids"]
+
+
+class ContactBookBaseSerializer(serializers.ModelSerializer):
+    """
+    주소록의 기본이 되는 Serializer 클래스
+    해당 클래스를 상속받는 Serializer가 많으므로, 공통 요소가 아닌 이상은 편집하지 않는 것을 권장함
+    """
+
+    profile_image_url = serializers.URLField(help_text="프로필 이미지 URL")
     name = serializers.CharField(help_text="이름")
     email = serializers.EmailField(help_text="이메일")
     phone = serializers.CharField(help_text="전화번호")
     company = serializers.CharField(help_text="회사")
     position = serializers.CharField(help_text="직책")
-    labels = LabelListSerializer(
-        source="labeled_contact",
-        many=True,
-        read_only=True,
-        help_text="라벨 목록",
+    memo = serializers.CharField(help_text="메모")
+    address = serializers.CharField(help_text="주소")
+    birthday = serializers.DateField(
+        help_text="생일",
+        format="%Y-%m-%d",
+        input_formats=["%Y-%m-%d"],
     )
+    website_url = serializers.URLField(help_text="웹사이트 URL")
 
+    class Meta:
+        models = ContactBook
+
+
+class ContactBookListSerializer(
+    ContactLabelSerializer,
+    ContactBookBaseSerializer,
+    serializers.ModelSerializer,
+):
     class Meta:
         model = ContactBook
         fields = [
@@ -37,51 +72,12 @@ class ContactBookListSerializer(serializers.ModelSerializer):
             "company",
             "position",
             "profile_image_url",
-            "labels",
-        ]
+        ] + ContactLabelSerializer.Meta.fields
 
 
-class ContactLabelListInputSerializer(serializers.Serializer):
-    label_ids = serializers.ListField(
-        write_only=True,
-        required=False,
-        child=serializers.IntegerField(),
-        help_text="라벨 ID",
-    )
-
-    class Meta:
-        fields = ["label_ids"]
-
-
-class ContactBookUpdateDeleteSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(
-        max_length=50, required=True, help_text="이름"
-    )
-    phone = PhoneNumberField(required=True, help_text="전화번호", region="KR")
-    email = serializers.EmailField(required=False, help_text="이메일")
-    company = serializers.CharField(
-        max_length=50, required=False, help_text="회사"
-    )
-    position = serializers.CharField(
-        max_length=50, required=False, help_text="직책"
-    )
-    memo = serializers.CharField(required=False, help_text="메모")
-    profile_image_url = serializers.URLField(
-        required=False, help_text="프로필 이미지 URL"
-    )
-    address = serializers.CharField(
-        max_length=100, required=False, help_text="주소"
-    )
-    birthday = serializers.DateField(
-        required=False,
-        help_text="생일",
-        format="%Y-%m-%d",
-        input_formats=["%Y-%m-%d"],
-    )
-    website_url = serializers.URLField(
-        required=False, help_text="웹사이트 URL"
-    )
-
+class ContactBookUpdateDeleteSerializer(
+    ContactBookBaseSerializer, serializers.ModelSerializer
+):
     def update(self, instance, validated_data):
         return super().update(instance, validated_data)
 
@@ -103,15 +99,10 @@ class ContactBookUpdateDeleteSerializer(serializers.ModelSerializer):
 
 
 class ContactBookRetrieveSerializer(
-    ContactLabelListInputSerializer,
-    ContactBookUpdateDeleteSerializer,
+    ContactLabelSerializer,
+    ContactBookBaseSerializer,
+    serializers.ModelSerializer,
 ):
-    labels = LabelListSerializer(
-        source="labeled_contact",
-        many=True,
-        read_only=True,
-        help_text="라벨 목록",
-    )
 
     def create(self, validated_data):
         with transaction.atomic():
@@ -125,20 +116,28 @@ class ContactBookRetrieveSerializer(
                     )
         return instance
 
-    class Meta(
-        ContactLabelListInputSerializer.Meta,
-        ContactBookUpdateDeleteSerializer.Meta,
-    ):
-        list_serializer_class = LabelListSerializer
+    class Meta:
+        model = ContactBook
+        fields = [
+            "id",
+            "name",
+            "email",
+            "phone",
+            "company",
+            "position",
+            "memo",
+            "profile_image_url",
+            "address",
+            "birthday",
+            "website_url",
+        ] + ContactLabelSerializer.Meta.fields
 
-        contact_book_meta = ContactBookUpdateDeleteSerializer.Meta.fields
-        label_list_meta = ContactLabelListInputSerializer.Meta.fields
-        fields = contact_book_meta + label_list_meta + ["labels"]
 
-
-class ContactBookLabelCreateSerializer(ContactLabelListInputSerializer):
-    class Meta(ContactLabelListInputSerializer.Meta):
-        fields = ContactLabelListInputSerializer.Meta.fields
+class ContactBookLabelCreateSerializer(
+    ContactLabelSerializer, serializers.Serializer
+):
+    class Meta:
+        fields = ContactLabelSerializer.Meta.fields
 
     def create(self, validated_data):
         with transaction.atomic():
@@ -148,7 +147,7 @@ class ContactBookLabelCreateSerializer(ContactLabelListInputSerializer):
             ).get(id=contact_id)
             labels = validated_data.pop("label_ids", [])
             labels: list[Label] = (
-                Label.owner(validated_data["owner"])
+                Label.objects.owner(validated_data["owner"])
                 .filter(id__in=labels)
                 .only("id")
             )
--- backend/apps/label/serializers.py
+++ backend/apps/label/serializers.py
@@ -15,29 +15,3 @@ class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
         fields = ["id", "name"]
-
-
-class LabelListSerializer(serializers.ModelSerializer):
-    """
-    연락처 정보에서 중첩된 구문으로 라벨 정보를 보여주기 위한 Serializer
-    """
-
-    id = serializers.IntegerField(source="label.id", help_text="라벨 ID")
-    name = serializers.CharField(
-        source="label.name",
-        max_length=50,
-        read_only=True,
-        help_text="라벨 이름",
-    )
-
-    def create(self, validated_data):
-        validated_data["owner"] = self.context["request"].user
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        validated_data["owner"] = self.context["request"].user
-        return super().update(instance, validated_data)
-
-    class Meta:
-        model = Label
-        fields = ["id", "name"]
