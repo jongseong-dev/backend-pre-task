043e646265e209fb7fb061069b037f715baba674
jslee <dlwhdtjd098@gmail.com>
2024-08-05 21:18:25 +0900

    modify: 필요한 api method를 추리고, api docs 정리

--- backend/apps/label/views.py
+++ backend/apps/label/views.py
@@ -1,6 +1,5 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import ModelViewSet
-
 from apps.label.models import Label
 from apps.label.serializers import LabelSerializer
 
@@ -17,5 +16,18 @@ class LabelViewSet(ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user)
 
+    @extend_schema(
+        summary="라벨 생성",
+        description="라벨을 생성하는 API",
+        tags=["Label"],
+    )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="라벨 조회",
+        description="본인이 생성한 라벨을 조회하는 API",
+        tags=["Label"],
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
